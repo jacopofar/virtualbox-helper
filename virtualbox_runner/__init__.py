8584d759252002bef57afffd3d814650d1063cbf
Jacopo Farina <jacopo.farina@flixbus.com>
2020-10-20 18:44:08 +0200

    Nicer tests, start matching experiments

--- virtualbox_runner/__init__.py
+++ virtualbox_runner/__init__.py
@@ -1,27 +1,46 @@
 import atexit
 from subprocess import (
     Popen,
-    PIPE,
-    STDOUT,
     TimeoutExpired,
     )
+from time import sleep
 from typing import Optional
 
 import remotevbox
 from remotevbox.machine import IMachine
+import cv2 as cv
+import numpy as np
 
 _server_proc: Optional[Popen] = None
 
 
 def ensure_server_running():
+    """Start a VirtualBox SOAP server.
+
+    This is mega ugly. This server by default logs but doesn't exit
+    in case the port is already in use, and this error is ent to stdout.
+
+    So, this only starts it and doesn't care.
+
+    Then, the process is terminated (SIGTERM, and after 3 seconds SIGKILL)
+    using the atexit hook.
+
+    Calling the function multiple times and/or when the server is already
+    running will start extra servers doing nothing, they are however quite
+    light and should be all terminated at exit.
+    """
     _server_proc = Popen("vboxwebsrv", shell=True)
     try:
         _server_proc.wait(2)
         raise ValueError('Cannot start the server!')
     except TimeoutExpired:
         # this is the normal case, it should just remain running
-        def terminate_vboxserver(proc=_server_proc):
+        def terminate_vboxserver(proc: Popen = _server_proc):
             proc.terminate()
+            sleep(3)
+            if proc.poll() is not None:
+                print('Process survived the SIGTERM, killing it!')
+                proc.kill()
         atexit.register(terminate_vboxserver)
 
 
@@ -30,11 +49,31 @@ def get_machine(
     password: str,
     machine_name: str,
     server_addr="http://127.0.0.1:18083",
-    start=True
+    start=False
         ) -> IMachine:
+    """Return the IMachine instance from a server with a given name.
+
+    If start is true, the machine is started as well.
+    """
 
     vbox = remotevbox.connect(server_addr, user, password)
     machine = vbox.get_machine(machine_name)
     if start:
         machine.launch()
     return machine
+
+
+def detect_fragment(screenshot_data: bytes, fragment: str) -> float:
+    img_rgb = cv.imdecode(np.frombuffer(screenshot_data, np.uint8), cv.IMREAD_COLOR)
+    template = cv.imread(fragment, cv.IMREAD_COLOR)
+    w, h, _ = template.shape
+    res = cv.matchTemplate(img_rgb, template, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img_rgb, top_left, bottom_right, (0, 0, 255), 2)
+
+    cv.imwrite('match.png', img_rgb)
+    # cv.imshow('image', img_rgb)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
