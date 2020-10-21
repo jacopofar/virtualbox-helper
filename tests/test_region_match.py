ee9ccd578f39eec5d6732a8a966f3e2714ebc27e
Jacopo Farina <jacopo.farina@flixbus.com>
2020-10-21 19:42:22 +0200

    Add tests and helper for region matching

---
+++ tests/test_region_match.py
@@ -0,0 +1,33 @@
+import pytest
+
+from virtualbox_runner import detect_fragment
+
+
+def test_region_match(tmpdir):
+    with open('tests/match_data/debian_grub.png', 'rb') as fp:
+        screenshot_data = fp.read()
+
+    detection = detect_fragment(screenshot_data, 'tests/match_data/grub_fragment.png')
+    assert detection is not None
+    # it's basically an exact match
+    assert detection[0] > 0.9
+    assert detection[1] == (19, 59)
+    assert detection[2] == (172, 114)
+    target_file = tmpdir.join('grub_match_test.png')
+    assert detection == detect_fragment(
+        screenshot_data,
+        'tests/match_data/grub_fragment.png',
+        store_match=str(target_file)
+        )
+    assert target_file.size() > 30_000
+
+    # no match at all
+    no_match = detect_fragment(screenshot_data, 'tests/match_data/random_image.png')
+    assert no_match is None
+
+    # force a match by lowering the threshold
+    forced_match = detect_fragment(screenshot_data, 'tests/match_data/random_image.png', threshold=0.001)
+    assert forced_match is not None
+
+    with pytest.raises(FileNotFoundError):
+        detect_fragment(screenshot_data, 'non_existing_file.png')
--- virtualbox_runner/__init__.py
+++ virtualbox_runner/__init__.py
@@ -4,7 +4,7 @@ from subprocess import (
     TimeoutExpired,
     )
 from time import sleep
-from typing import Optional
+from typing import Optional, Tuple
 
 import remotevbox
 from remotevbox.machine import IMachine
@@ -63,17 +63,38 @@ def get_machine(
     return machine
 
 
-def detect_fragment(screenshot_data: bytes, fragment: str) -> float:
+def detect_fragment(
+    screenshot_data: bytes,
+    fragment: str,
+    threshold: float = 0.8,
+    store_match: Optional[str] = None
+        ) -> Optional[Tuple[float, Tuple[int, int], Tuple[int, int]]]:
+    """Detect the presence and position of a given fragment in the sceenshot.
+
+    If the fragment is not found, None is returned.
+    If it's found, a tuple is returned containing the match probability
+    and the coordinates of the top left and bottom right corners.
+
+    If store_match is given, a file with that name will be created showing
+    the matched region as a red rectangle.
+    """
     img_rgb = cv.imdecode(np.frombuffer(screenshot_data, np.uint8), cv.IMREAD_COLOR)
     template = cv.imread(fragment, cv.IMREAD_COLOR)
+    if template is None:
+        raise FileNotFoundError(f'Cannot find {fragment}')
     w, h, _ = template.shape
     res = cv.matchTemplate(img_rgb, template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img_rgb, top_left, bottom_right, (0, 0, 255), 2)
 
-    cv.imwrite('match.png', img_rgb)
+    if max_val < threshold:
+        return None
+
+    top_left = max_loc
+    bottom_right = (top_left[0] + h, top_left[1] + w)
+    if store_match is not None:
+        cv.rectangle(img_rgb, top_left, bottom_right, (0, 0, 255), 2)
+        cv.imwrite(store_match, img_rgb)
+    return (max_val, top_left, bottom_right)
     # cv.imshow('image', img_rgb)
     # cv.waitKey(0)
     # cv.destroyAllWindows()
